8f001266e29fb1b59347879fc3a9aeac3e6bb07b
Artem Sushko <temagic85@gmail.com>
2025-07-20 12:38:50 +0300

    init: start RainbowFrame task

---
+++ RainbowFrame/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render(): React.ReactNode {
+    return (
+      <>
+        <div className="container mx-auto px-3 sm:px-4 lg:px-5"></div>
+      </>
+    );
+  }
+}
