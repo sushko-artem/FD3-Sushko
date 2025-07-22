fede6b29e9e2e9d476dffcfccdcaced2026017aa
Artem Sushko <temagic85@gmail.com>
2025-07-22 21:02:54 +0300

    fix: delete animation

--- RainbowFrame/src/components/RainbowFrame.tsx
+++ RainbowFrame/src/components/RainbowFrame.tsx
@@ -1,54 +1,12 @@
 import React from "react";
-import { LinkedListQueue } from "@shared/data_structures/queue";
-import { shallowEqual } from "shallow-equal";
 
 type FramePropsType = {
   colors: string[];
   children: React.ReactNode;
 };
-
-type FrameStateType = {
-  colors: string[];
-};
-
-export default class RainbowFrame extends React.Component<
-  FramePropsType,
-  FrameStateType
-> {
-  private queue: LinkedListQueue = new LinkedListQueue();
-  private interval: NodeJS.Timeout | null = null;
-  constructor(props: FramePropsType) {
-    super(props);
-    this.props.colors.forEach((item) => this.queue.enqueue(item));
-  }
-  state = {
-    colors: this.queue.list,
-  };
-
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      const firstItem = this.queue.dequeue() as string;
-      this.queue.enqueue(firstItem);
-      this.setState({
-        colors: this.queue.list,
-      });
-    }, 500);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval!);
-  }
-
-  componentDidUpdate(prevState: Readonly<FrameStateType>) {
-    if (shallowEqual(prevState, this.state)) {
-      this.setState({
-        colors: this.state.colors,
-      });
-    }
-  }
-
+export default class RainbowFrame extends React.Component<FramePropsType> {
   getFrame = () =>
-    this.state.colors.reduce(
+    this.props.colors.reduce(
       (accum, color, index) => (
         <div
           key={index}
