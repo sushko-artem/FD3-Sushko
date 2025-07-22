a3ea3d96fda51ce9f7fab87c361e152f02f66a66
Artem Sushko <temagic85@gmail.com>
2025-07-22 20:51:39 +0300

    fix: correct computed length property

--- br2jsx/src/components/BR2JSX.tsx
+++ br2jsx/src/components/BR2JSX.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 
 export default class BR2JSX extends React.Component<{ text: string }> {
+  text = this.props.text.replace(/<\w+[ *\\/]*>/g, ",").split(",");
   getJSXfromText = () =>
-    this.props.text
-      .replace(/<\w+[ *\\/]*>/g, ",")
-      .split(",")
-      .reduce(
-        (accum, str, index) =>
-          index < this.props.text.length - 1
-            ? accum.concat([str, <br key={index} />])
-            : accum.concat([str]),
-        [] as (string | React.ReactNode)[]
-      );
+    this.text.reduce(
+      (accum, str, index) =>
+        index < this.text.length - 1
+          ? accum.concat([str, <br key={index} />])
+          : accum.concat([str]),
+      [] as (string | React.ReactNode)[]
+    );
   render(): React.ReactNode {
     return (
       <div className="bg-amber-200 font-bold w-fit text-center mx-auto p-5">
