e29fcbbb43defb8f8773fc54c768003995e6bee5
Artem Sushko <temagic85@gmail.com>
2025-08-05 15:07:14 +0300

    fix: correct state changes

--- MobileHook/src/components/Mobile.tsx
+++ MobileHook/src/components/Mobile.tsx
@@ -13,7 +13,6 @@ type ClientsType = IClients["clients"];
 
 export const Mobile = () => {
   console.log("render Mobile");
-  // const [clients, setClients] = useState<ClientsType>([])
   const [clients, setClients] = useState<ClientsType>([
     ...(DATA.clients as ClientsType),
   ]);
@@ -29,58 +28,28 @@ export const Mobile = () => {
       emitter.off("filter", handleFilter);
       emitter.off("edit", handleEdit);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const filteredClients = useMemo(() => {
-    const currentClients = [...clients];
     switch (filter) {
       case "active":
-        return currentClients.filter((item) => item.balance >= 0);
+        return clients.filter((item) => item.balance >= 0);
       case "blocked":
-        return currentClients.filter((item) => item.balance < 0);
+        return clients.filter((item) => item.balance < 0);
       default:
-        return currentClients;
+        return clients;
     }
   }, [clients, filter]);
 
-  {
-    /*
-  ---> Так Mobile рендерится по 2 раза. Сначала с пустым clients 
-  (useState<ClientsType>([])), а потом с новым стейтом clients. Если бы 
-  получали данные с backend, использовали бы useEffect.
-
-  useEffect(() => {
-    const clients = [...(DATA.clients as ClientsType)];
-    switch (filter) {
-      case "active":
-        setClients(clients.filter((item) => item.balance >= 0));
-        break;
-      case "blocked":
-        setClients(clients.filter((item) => item.balance < 0));
-        break;
-      default:
-        setClients(clients);
-    }
-  }, [filter]);
-*/
-  }
-
   const handleFilter = (filter: "all" | "blocked" | "active") => {
-    // React использует механизм "bailout" (прекращение обновлений) и перестает
-    // рендерить компонент после первого повторного нажатия. Из-за этого только
-    // при первом повторном нажатии на уже обработанное событие происходит
-    // рендер Mobile. Все последующие - игнорируются. Иначе, не понятно откуда
-    // этот лишний рендер и как избавиться от него.
     setFilter(filter);
   };
 
   const handleEdit = (payload: ClientsType[number] | string) => {
     if (typeof payload === "object") {
-      const currentClients = [
-        ...clients.map((item) => (item.id === payload.id ? payload : item)),
-      ];
-      setClients(currentClients);
+      setClients((prevClients) =>
+        prevClients.map((item) => (item.id === payload.id ? payload : item))
+      );
       setClientId(null);
     } else setClientId(payload);
   };
@@ -93,13 +62,11 @@ export const Mobile = () => {
       secondName: "",
       balance: 0,
     };
-    const currentClients = [...clients, { ...newClient }];
-    setClients(currentClients);
+    setClients((prevClients) => [...prevClients, newClient]);
   };
 
   const handleDelete = (id: string) => {
-    const currentClients = [...clients.filter((item) => item.id !== id)];
-    setClients(currentClients);
+    setClients((prevClients) => prevClients.filter((item) => item.id !== id));
   };
 
   return (
